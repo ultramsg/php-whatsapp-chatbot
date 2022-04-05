1e8b4b70915a97c89e31adde4a1b1a7bd584ea45
Ultramsg <95187366+ultramsg@users.noreply.github.com>
2022-04-05 15:04:55 +0300

    Update example.php

--- example.php
+++ example.php
@@ -13,7 +13,7 @@ class ultraMsgChatBot
         //require_once ('vendor/autoload.php'); // if you use Composer 
         //composer require ultramsg/whatsapp-php-sdk
 
-        require_once('ultramsg.class.php'); //Latest 2.0.1 stable
+        require_once('ultramsg.class.php'); //Latest 2.0.3 stable
         //Include a dictionary to generate random words and sentences
         require_once('ultramsg-dictionary.php');
 
